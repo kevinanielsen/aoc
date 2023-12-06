a9860367a0d1e22dc0c3b316625f7504e20a2622
kevinanielsen <keveran@gmail.com>
2023-12-06 09:21:08 +0100

    add template for reading input.txt

---
+++ src/template/template.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		var currentLine string = scanner.Text()
+	}
+}
